421cffe453bcc812bc6e3727cd69789ddd82583d
maferamirez97 <maferamcif@gmail.com>
2020-12-06 09:57:47 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,30 +9,30 @@ bluetooth.startAccelerometerService()
 bluetooth.startUartService()
 bluetooth.startButtonService()
 basic.forever(function () {
-bluetooth.uartWriteNumber(input.lightLevel())
-x = input.acceleration(Dimension.X)
-y = input.acceleration(Dimension.Y)
-isEast = x > 90
-isWest = x < -90
-isNorth = y < -90
-isSouth = y > 90
-if (isEast && isNorth) {
-basic.showArrow(ArrowNames.NorthEast)
-} else if (isEast && isSouth) {
-basic.showArrow(ArrowNames.SouthEast)
-} else if (isWest && isNorth) {
-basic.showArrow(ArrowNames.NorthWest)
-} else if (isWest && isSouth) {
-basic.showArrow(ArrowNames.SouthWest)
-} else if (isEast) {
-basic.showArrow(ArrowNames.East)
-} else if (isNorth) {
-basic.showArrow(ArrowNames.North)
-} else if (isSouth) {
-basic.showArrow(ArrowNames.South)
-} else if (isWest) {
-basic.showArrow(ArrowNames.West)
-} else {
-basic.showIcon(IconNames.SmallDiamond)
-}
+    bluetooth.uartWriteNumber(input.lightLevel())
+    x = input.acceleration(Dimension.X)
+    y = input.acceleration(Dimension.Y)
+    isEast = x > 90
+    isWest = x < -90
+    isNorth = y < -90
+    isSouth = y > 90
+    if (isEast && isNorth) {
+        basic.showArrow(ArrowNames.East)
+    } else if (isEast && isSouth) {
+        basic.showArrow(ArrowNames.East)
+    } else if (isWest && isNorth) {
+        basic.showArrow(ArrowNames.West)
+    } else if (isWest && isSouth) {
+        basic.showArrow(ArrowNames.West)
+    } else if (isEast) {
+        basic.showArrow(ArrowNames.East)
+    } else if (isNorth) {
+        basic.showArrow(ArrowNames.North)
+    } else if (isSouth) {
+        basic.showArrow(ArrowNames.South)
+    } else if (isWest) {
+        basic.showArrow(ArrowNames.West)
+    } else {
+        basic.showIcon(IconNames.SmallDiamond)
+    }
 })
